ca6985048a1d615f195e667d225e09505c562703
clerki <53558451+clerki@users.noreply.github.com>
2022-06-14 16:10:00 +0800

    Update odAuthTokenStore.ts

--- utils/odAuthTokenStore.ts
+++ utils/odAuthTokenStore.ts
@@ -3,10 +3,12 @@ import Redis from 'ioredis'
 // Persistent key-value store is provided by Redis, hosted on Upstash
 // https://vercel.com/integrations/upstash
 const kv = new Redis(process.env.REDIS_URL)
+const yaccess_token = process.env.AC || 'access_token'
+const yrefresh_token = process.env.RE || 'refresh_token'
 
 export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
-  const accessToken = await kv.get('access_token')
-  const refreshToken = await kv.get('refresh_token')
+  const accessToken = await kv.get(yaccess_token)
+  const refreshToken = await kv.get(yrefresh_token)
 
   return {
     accessToken,
@@ -23,6 +25,6 @@ export async function storeOdAuthTokens({
   accessTokenExpiry: number
   refreshToken: string
 }): Promise<void> {
-  await kv.set('access_token', accessToken, 'ex', accessTokenExpiry)
-  await kv.set('refresh_token', refreshToken)
+  await kv.set(yaccess_token, accessToken, 'ex', accessTokenExpiry)
+  await kv.set(yrefresh_token, refreshToken)
 }
